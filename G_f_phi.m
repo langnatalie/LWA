9633be22188d2b42e4848d1a12c21e52a9bdba73
Natalie Lang <langn@post.bgu.ac.il>
2025-03-20 13:13:30 +0200

    Add files via upload

---
+++ G_f_phi.m
@@ -0,0 +1,12 @@
+function G=G_f_phi(f,phi,b,L)
+%f is a scalar, phi [rad] can be multi-dimantional size [1,#radians]
+
+% f can be size [#frecs,1]
+c=3*10^8;
+k0=2*pi*f/c; %[40,1]
+betta=k0.*sqrt(1-((c./(2*b*f)).^2));
+ones_vect=ones(1,length(phi));
+arg=L./2.*(betta*ones_vect-k0*cos(phi));
+G=sinc(arg);   % sinc defined sin(pi*x)/(pi*x)
+
+end
